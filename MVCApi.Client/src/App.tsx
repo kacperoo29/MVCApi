524a0397cf393f143b9ddad350df89bfc337d196
Kacper Bajeński <erednay29@gmail.com>
2021-11-15 11:24:26 +0100

    Use typescript instead of js

---
+++ MVCApi.Client/src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+
+//import axios from 'axios'
+import React, { Component, useState } from 'react'
+import { CustomerDto, CustomerApi } from './api/index'
+
+export default function App() {
+  const [customers, setCustomers] = useState<CustomerDto[] | []>([])
+  const api = new CustomerApi();
+
+  api.apiCustomerGetAllCustomersGet().then(response => setCustomers(response))
+
+  return (
+    <ul>
+      {customers.map(customer => <li>{customer.id}</li>)}
+    </ul>
+  )
+}
---
+++ MVCApi.Client/src/api/apis/index.ts
@@ -0,0 +1,6 @@
+export * from './CartApi';
+export * from './CategoryApi';
+export * from './CustomerApi';
+export * from './OrderApi';
+export * from './ProductApi';
+export * from './UserApi';
