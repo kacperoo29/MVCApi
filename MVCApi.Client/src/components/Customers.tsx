a106d8c0eb1bd9284ad50584080ce90d58c84e0a
Kacper Bajeński <erednay29@gmail.com>
2021-12-04 15:17:09 +0100

    Add dropdown with customer address/contactinfo

--- MVCApi.Client/src/components/Customers.tsx
+++ MVCApi.Client/src/components/Customers.tsx
@@ -1,34 +1,49 @@
 import React, { useEffect, useState } from 'react'
 import { CustomerDto, CustomerApi } from '../api/index'
-import { Table } from 'react-bootstrap'
+import { Dropdown } from 'react-bootstrap'
 import { useIntl } from 'react-intl'
 
-export default function Customers (props: any) {
-  const [customers, setCustomers] = useState<CustomerDto[] | []>([])
-  const intl = useIntl()
-  
-  useEffect((api = new CustomerApi()) => {
-    api.apiCustomerGetAllCustomersGet().then(response => {
-      setCustomers(response)
-    })
-  }, [])
+export default function Customers(props: any) {
+	const [customers, setCustomers] = useState<CustomerDto[] | []>([])
+	const intl = useIntl()
 
-  return (<Table striped bordered hover>
-    <thead>
-      <tr>
-        <th>First name</th>
-        <th>Last name</th>
-        <th>Date of birth</th>
-      </tr>
-    </thead>
-    <tbody>
-      {customers.map(customer => (
-        <tr key={customer.id}>
-          <td>{customer.firstName}</td>
-          <td>{customer.lastName}</td>
-          <td>{new Date(customer.dateOfBirth!).toLocaleDateString(intl.locale)}</td>
-        </tr>
-      ))}
-    </tbody>
-  </Table>)
+	useEffect((api = new CustomerApi()) => {
+		api.apiCustomerGetAllCustomersGet().then(response => {
+			setCustomers(response)
+		})
+	}, [])
+
+	return (<>
+		{customers.map((customer, id) => (
+			<div key={customer.id} className="rounded border p-2">
+				<div className="row">
+					<p className="col-md-4">First name: {customer.firstName}</p>
+					<p className="col-md-4">Last name: {customer.lastName}</p>
+					<p className="col-md-4">Date of birth: {new Date(customer.dateOfBirth!).toLocaleDateString(intl.locale)}</p>
+				</div>
+				<div className="row">					
+					<Dropdown className="col-md-6">
+						<Dropdown.Toggle id={'addresses_' + id}>
+							Addresses
+						</Dropdown.Toggle>
+						<Dropdown.Menu>
+							{customer.addresses && customer.addresses.map(address =>
+								<Dropdown.Item key={address.id}>{address.country + ', ' + address.postCode + ' ' + address.city + ', ' + address.street + ' ' + address.streetNumber}</Dropdown.Item>
+							)}
+						</Dropdown.Menu>
+					</Dropdown>
+					<Dropdown className="col-md-6">
+						<Dropdown.Toggle id={'contacts_' + id}>
+							Contact information
+						</Dropdown.Toggle>
+						<Dropdown.Menu>
+							{customer.contactInfos && customer.contactInfos.map(contact =>
+								<Dropdown.Item key={contact.id}>{contact.email + ', ' + contact.phoneNumber}</Dropdown.Item>
+							)}
+						</Dropdown.Menu>
+					</Dropdown>
+				</div>
+			</div>
+		))}
+	</>)
 }
