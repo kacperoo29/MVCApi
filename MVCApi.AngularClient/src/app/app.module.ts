20e63b81ba8b6e48e4908a0f8fc7293e45c4c0e8
Kacper Bajeński <erednay29@gmail.com>
2022-01-09 12:29:27 +0100

    Example api call in angular

--- MVCApi.AngularClient/src/app/app.module.ts
+++ MVCApi.AngularClient/src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
